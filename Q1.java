c4187292654c5b581a3c46ffabcb1329257e48d0
Ailton <ailtongfd@gmail.com>
2023-03-09 21:32:14 -0300

    Adicionando questões 1 e 5

---
+++ Q1.java
@@ -0,0 +1,21 @@
+public class Q1 {
+    
+    public static void main(String[] args) {
+        
+
+        int ind = 13;
+
+        int sum = 0;
+
+        int k = 0;
+
+        while (k < ind) {
+
+            k = k+1;
+
+            sum = sum + k;
+        }
+
+        System.out.println(sum);
+    }
+}
---
+++ Q5.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+public class Q5 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite uma string: ");
+        String string = scanner.nextLine();
+        
+        System.out.println("Sua string: " + string);
+        
+
+        System.out.println("String invertida: " + reverseString(string));
+
+    }
+
+    public static String reverseString (String string) {
+
+        char[] array = new char[string.length()];
+
+        int count = 1;
+
+        for (int i = 0; i < string.length(); i++) {
+
+            array[i] = string.charAt(string.length() - count);
+            count = count + 1;
+        }
+
+        String invertedString = String.valueOf(array);
+
+        return invertedString;
+    }
+    
+}
